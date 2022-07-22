d7efb7ef691d15aeb8ef85d55570fc9a3e8767aa
GabrielVoica <gabi_voica@hotmail.com>
2022-07-22 12:38:21 +0200

    Finished banning middleware functonality

--- app/Http/Middleware/CheckUserIsNotBanned.php
+++ app/Http/Middleware/CheckUserIsNotBanned.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use App\Models\Block;
+use Illuminate\Support\Facades\Auth;
 
 class CheckUserIsNotBanned
 {
@@ -17,11 +19,20 @@ class CheckUserIsNotBanned
     public function handle(Request $request, Closure $next)
     {
 
-        //TODO Implement banned middleware system
-        return response()->json([
-            'status' => 0,
-            'msg' => 'User is banned in this room'
-        ],400);
+        //TODO Finish middleware system
+        $block = Block::where('user_id','=',Auth::user()->id)->where('room_id','=',$request->route('id'))->first();
+
+        if(isset($block)){
+            return response()->json([
+                'status' => 0,
+                'msg' => 'User is banned in this room',
+                'data' => [
+                    'reason' => $block->reason,
+                    'date' => $block->created_at
+                ]
+            ],400);
+        }
+
 
         return $next($request);
     }
